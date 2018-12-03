9a18e7dd23441eaff9869101928e01243c45df2d
eddyh <eddyh@192.168.56.1>
2018-12-03 19:36:06 +0000

    Kata 5 Parte 1 versión 3

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,26 +1,33 @@
 package kata5p1;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Iterator;
+import java.util.List;
+import static kata5p1.MailListReader.read;
 
 public class Kata5P1 {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
         
-        String url  = "jdbc:sqlite:KATA5.db";
-        String sql = "CREATE TABLE EMAIL (\n"
-                + "Id integer PRIMARY KEY AUTOINCREMENT,\n"
-                + "Mail text NOT NULL)";
-        
-        try{ Connection conn = DriverManager.getConnection(url);
-            Statement stm = conn.createStatement();
-            stm.execute(sql);
-            System.out.println("Se ha creado la tabla EMAIL");
-        } catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
+       String url = "jdbc:sqlite:KATA5.db";
+       String path = "C:\\Users\\eddyh\\Desktop\\Kata5P1\\email.txt";
+       List<String> emails = read(path);
+       String sql= "INSERT INTO EMAIL(Mail) VALUES(?)";
+       try{ Connection conn = DriverManager.getConnection(url);
+            PreparedStatement pst = conn.prepareStatement(sql);
+            Iterator<String> it = emails.iterator();
+            while(it.hasNext()){
+                pst.setString(1, it.next());
+                pst.executeUpdate();
+            }
+       }catch(SQLException e) {
+           System.out.println(e.getMessage());
+       }
     }
 }
